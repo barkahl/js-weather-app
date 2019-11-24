1b8ac6775cd34e9cd185f7d2ef261404d4086975
Bartosz Kahl <bartosz.kahl@gmail.com>
2019-11-24 04:12:01 +0100

    Initial structure

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const outputDir = path.join(__dirname, 'build/');
+
+const isProd = process.env.NODE_ENV === 'production';
+
+module.exports = {
+    entry: [
+        'regenerator-runtime/runtime',
+        './src/Index.js',
+    ],
+    mode: isProd ? 'production' : 'development',
+    output: {
+        path: outputDir,
+        filename: 'Index.js',
+        publicPath: '/',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'src/index.html',
+            inject: true
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.scss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    'style-loader',
+                    // Translates CSS into CommonJS
+                    'css-loader',
+                    // Compiles Sass to CSS
+                    'sass-loader',
+                ],
+            },
+        ]
+    }
+};
